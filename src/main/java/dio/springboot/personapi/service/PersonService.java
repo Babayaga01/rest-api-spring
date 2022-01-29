48813440d4f60d692f651496ce520dfc315bdea5
Henrique <henriquegr91@gmail.com>
2022-01-29 20:13:40 -0300

    Developed person creat operation

---
+++ src/main/java/dio/springboot/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package dio.springboot.personapi.service;
+
+import dio.springboot.personapi.dto.MessageResponseDTO;
+import dio.springboot.personapi.entity.Person;
+import dio.springboot.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
